9ae3377a12c65fdb3419ae123fe9ad2b09b8ce72
Jimmy Gitonga <jimmy@afroshok.com>
2021-01-07 06:30:52 +0300

    FEATURE: Add steps to be used to progress the form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,66 @@
+import React, { useState } from 'react';
+import { Card, CardContent, Typography, Button } from '@material-ui/core';
+import { Field, Form, Formik, FormikConfig, FormikValues } from 'formik';
+import { CheckboxWithLabel, TextField } from 'formik-material-ui';
+import { object, mixed, number } from 'yup';
+
 export default function Home() {
-  return <div>Hello Youtube!!!</div>;
+  return (
+    <Card>
+      <CardContent>
+        <FormikStepper
+          validationSchema={object({
+            money: mixed().when('millionaire', {
+              is: true,
+              then: number().required().min(1_000_000, 'You did say you are a millionaire, so you need 1 million'),
+              otherwise: number().required()
+            })
+          })}
+          initialValues={{
+          firstName: '',
+          lastName: '',
+          millionaire: false,
+          money: 0,
+          description: ''
+        }} onSubmit={() => {}}>
+          <div>
+            <Field name='firstName' component={TextField} label="First Name" />
+            <Field name='lastName' component={TextField} label="Last Name" />
+            <Field name='millionaire' type="checkbox" component={CheckboxWithLabel} Label={{ label: 'I am a millionaire'}} />
+          </div>
+          <div>
+            <Field name='money' type='number' component={TextField} label="All the money I have" />
+          </div>
+          <div>
+            <Field name='description' component={TextField} label="Description" />
+          </div>
+        </FormikStepper>
+      </CardContent>
+    </Card>
+  );
+}
+
+export function FormikStepper({children, ...props}: FormikConfig<FormikValues>) {
+  const childrenArray = React.Children.toArray(children);
+  const [step, setStep] = useState(0);
+  const currentChild = childrenArray[step];
+
+  function isLastStep() {
+    return step === childrenArray.length - 1;
+  }
+
+  return (
+    <Formik {...props} onSubmit={async(values, helpers) => {
+      if(isLastStep()) {
+        await props.onSubmit(values, helpers);
+      } else {
+        setStep(s => s+1);
+      }
+    }}>
+      <Form autoComplete="off">{currentChild}
+        {step > 0 ? <Button onClick={() => setStep(s => s-1)}>Back</Button> : null}
+        <Button type='submit'>{isLastStep() ? 'Done' : 'Next'}</Button>
+      </Form>
+    </Formik>
+  )
 }
